460a0df1a4bf8d2f10293ea59c662e6ede291a0c
YahyaQandel <yqandel@kwentra.com>
2025-03-17 14:03:58 +0200

    update game url

--- cypress/e2e/tictactoe.cy.ts
+++ cypress/e2e/tictactoe.cy.ts
@@ -1,6 +1,6 @@
 describe('tic tac toe spec', () => {
   it('user wins when a row is complete', () => {
-    cy.visit('http://tictactoe.automationtesting.tech');
+    cy.visit('https://testing-tictactoe-371959384592.europe-west1.run.app');
     cy.get('#square-0').click();
     cy.get('#square-3').click();
     cy.get('#square-1').click();
