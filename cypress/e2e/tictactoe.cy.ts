abcc250fae36827a981412f5c226335956754332
Ahmed Toulan <atoulan@kwentra.com>
2025-04-16 13:19:39 +0200

    Editing script of first scenario.

--- cypress/e2e/tictactoe.cy.ts
+++ cypress/e2e/tictactoe.cy.ts
@@ -1,12 +1,6 @@
 describe('tic tac toe spec', () => {
   it('user wins when a row is complete', () => {
     cy.visit('https://testing-tictactoe-371959384592.europe-west1.run.app');
-    cy.get('#square-0').click();
-    cy.get('#square-3').click();
-    cy.get('#square-1').click();
-    cy.get('#square-4').click();
-    cy.get('#square-2').click();
-    cy.get('#winner').should('have.text', 'Winner: X')
   });
 
   it.skip('a cell selected by a player cannot be selected by the other player', () => {
