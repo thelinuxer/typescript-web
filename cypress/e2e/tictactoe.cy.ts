e633e6208b21f9f54b94ba09c4a63524ae548e70
Ahmed Toulan <atoulan@kwentra.com>
2025-04-16 13:19:58 +0200

    Add visit in test scenarions.

--- cypress/e2e/tictactoe.cy.ts
+++ cypress/e2e/tictactoe.cy.ts
@@ -4,10 +4,10 @@ describe('tic tac toe spec', () => {
   });
 
   it.skip('a cell selected by a player cannot be selected by the other player', () => {
-
+    cy.visit('https://testing-tictactoe-371959384592.europe-west1.run.app');
   });
 
   it.skip('go back to certain move', () => {
-
+    cy.visit('https://testing-tictactoe-371959384592.europe-west1.run.app');
   });
 });
